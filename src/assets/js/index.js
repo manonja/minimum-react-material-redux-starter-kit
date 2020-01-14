aa877ea0727d5f2dc1dffa0b0ba30ce00f75c080
Manon Jacquin <manon.jacquin@gmail.com>
2020-01-14 20:00:51 +0000

    Adds window.react to the global scope

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,6 +4,12 @@ import '../css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Here we put our React instance to the global scope. Make sure you do not put it 
+// into production and make sure that you close and open your console if the 
+// DEV-TOOLS does not display
+window.React = React; 
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
